d335c568decab939509b6175d751f598713f9cb0
Ziad Elshihy <elshihyziad@gmail.com>
2026-01-14 00:27:27 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/qa-portfolio/",
+  base: "/my_portfolio/",
 
   server: {
     host: "::",
