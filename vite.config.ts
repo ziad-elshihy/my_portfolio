7117a9dcf10e59e000394afaaf2c6b449eba28c0
Ziad Elshihy <elshihyziad@gmail.com>
2026-01-14 01:01:41 +0200

    Remove GitHub Pages and gh-pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/my_portfolio/",
-
+  base: "/",
   server: {
     host: "::",
     port: 8080,
